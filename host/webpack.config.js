d60d8285fe3f783aabb6fc39d444c785f60bbf5a
ddevarg <david.devargas@morningstar.com>
2023-04-03 10:27:14 -0500

    feat(host): use html-webpack-plugin

--- host/webpack.config.js
+++ host/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
@@ -7,6 +8,14 @@ module.exports = {
   },
   output: {
     filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  }
+    path: path.resolve(__dirname, 'dist'),
+    clean: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Cool Example'
+    })
+  ],
+  devtool: 'inline-source-map',
+  mode: 'development'
 }
